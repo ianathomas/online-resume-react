01da1a2c2bf689350ed376cc69ae8be70950c14d
IAN THOMAS <ianthomas9891@gmail.com>
2023-07-05 15:44:15 -0400

    made 3 individual tweets per show action

--- src/ResumeShow.jsx
+++ src/ResumeShow.jsx
@@ -1,6 +1,8 @@
 import { faker } from "@faker-js/faker";
 
-const randomWords = faker.word.words({ count: { min: 5, max: 10 } }); // adds 5 to 10 words for a "tweet"
+const randomTweet1 = faker.word.words({ count: { min: 5, max: 10 } });
+const randomTweet2 = faker.word.words({ count: { min: 5, max: 10 } });
+const randomTweet3 = faker.word.words({ count: { min: 5, max: 10 } }); // adds 5 to 10 words for a "tweet"
 
 export function ResumeShow(props) {
   return (
@@ -8,9 +10,9 @@ export function ResumeShow(props) {
       <h1>Resume information</h1>
       <p>Name: {props.resume.name}</p>
       <p>Qty: {props.resume.quantity}</p>
-      <p>Tweet: {randomWords} </p>
-      <p>Tweet: {randomWords} </p>
-      <p>Tweet: {randomWords} </p>
+      <p>Tweet: {randomTweet1} </p>
+      <p>Tweet: {randomTweet2} </p>
+      <p>Tweet: {randomTweet3} </p>
     </div>
   );
 }
