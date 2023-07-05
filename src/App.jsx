74a877d6b964a856b2082e7eb1d5091a57e04402
Paolo Maloles <jpaolomaloles@gmail.com>
2023-07-05 12:43:05 -0500

    ResumeIndex

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
+import { useState, useEffect } from "react";
+// import axios from "axios";
 
 function App() {
+  const [resumes, setResumes] = useState([]);
+
+  const dummydata = [
+    { id: 1, name: "aaa", quantity: 1 },
+    { id: 2, name: "bbb", quantity: 2 },
+    { id: 3, name: "ccc", quantity: 3 },
+  ];
+
+  const handleSetResumes = () => {
+    // axios.get(`http://localhost:3000`).then((response) => {
+    //   console.log("handleSetResumes");
+    //   setResumes(response.data);
+    // })
+    console.log("handleSetResumes");
+    setResumes(dummydata);
+  };
+
+  useEffect(handleSetResumes);
+
   return (
     <div>
       <Header />
-      <Content />
+      <Content resumes={resumes} />
       <Footer />
     </div>
   );
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,10 @@
-export function Content() {
+import { ResumeIndex } from "./ResumeIndex";
+
+export function Content(props) {
   return (
     <div>
       <h1>Welcome to React!</h1>
+      <ResumeIndex resumes={props.resumes} />
     </div>
   );
 }
